e37c9f204cd28dc62d2a5eaa0d7512a1e60986fa
Topper Bowers <topper@toppingdesign.com>
2023-06-10 16:52:25 +0200

    use the passed in conversation

--- core/src/conversationProcessor.ts
+++ core/src/conversationProcessor.ts
@@ -73,12 +73,12 @@ export class ConversationProcessor extends EventEmitter {
       name: "rambleAfterResponding",
       description:
         "If you want to continue talking, without waiting for a response. Use YES or NO as input.",
-      execute: (input: string, _soul, _conversation) => {
+      execute: (input, _soul, conversation) => {
         devLog(`executing ramble action with input: ${input}`);
         if (input.toLowerCase() === "no") {
           return;
         }
-        this.generatedThoughts.push(
+        conversation.generatedThoughts.push(
           new Thought({
             role: "assistant",
             entity: this.blueprint.name,
@@ -86,7 +86,7 @@ export class ConversationProcessor extends EventEmitter {
             content: "I want to ramble before they respond",
           })
         );
-        this.continueThinking();
+        conversation.continueThinking();
       },
     };
     return [rambleAction].concat(this.soul.actions);
