9955f24c1d3171b938f44c9776b38cea57e0d87b
Topper Bowers <topper@toppingdesign.com>
2023-06-27 10:23:24 +0200

    checked in storage by accident

--- core/src/storage/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Storage {
-  get(
-    key: string,
-    defaultValue: string | undefined
-  ): Promise<string | undefined>;
-  set(key: string, value: string | undefined): Promise<void>;
-  delete(key: string): Promise<void>;
-  exists(key: string): Promise<boolean>;
-}
